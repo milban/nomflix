31afec5957dd7e1dec37e086f3aae6cd52351956
JunHyeock Park <wnsgur6311@gmail.com>
2019-08-21 02:09:17 +0900

    React Router Basic

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react"
+import Router from "Components/Router"
+import Header from "Components/Header"
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <Router />
+    </Fragment>
+  )
+}
+
+export default App
