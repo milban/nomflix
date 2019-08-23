c1472833ecdd11729d7d4b15799e90ca3b7be4f6
JunHyeock Park <wnsgur6311@gmail.com>
2019-08-23 23:57:14 +0900

    Header SC

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from "react"
 import Router from "Components/Router"
-import "./reset.css"
+import GlobalStyles from "Components/GlobalStyles"
 
 function App() {
   return (
     <Fragment>
       <Router />
+      <GlobalStyles />
     </Fragment>
   )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,32 +1,51 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, withRouter } from "react-router-dom"
 import styled from "styled-components"
 
-const SHeader = styled.header``
+const SHeader = styled.header`
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding: 0px 10px;
+  background-color: rgba(20, 20, 20, 0.7);
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`
 
 const List = styled.ul`
   display: flex;
-  justify-content: space-around;
 `
 
-const Item = styled.li``
+const Item = styled.li`
+  width: 80px;
+  height: 50px;
+  border-bottom: 5px solid ${props => (props.current ? "#1abc9c" : "transparent")};
+`
 
 const SLink = styled(Link)`
   text-decoration: none;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
-export default () => (
+export default withRouter(({ location: { pathname } }) => (
   <SHeader>
     <List>
-      <Item>
+      <Item current={pathname === "/"}>
         <SLink to="/">Movies</SLink>
       </Item>
-      <Item>
+      <Item current={pathname === "/tv"}>
         <SLink to="/tv">TV</SLink>
       </Item>
-      <Item>
+      <Item current={pathname === "/search"}>
         <SLink to="/search">Search</SLink>
       </Item>
     </List>
   </SHeader>
-)
+))
