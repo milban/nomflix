5b600edad87c1ef1643141bea71845481ea25f3d
JunHyeock Park <wnsgur6311@gmail.com>
2019-08-27 18:12:44 +0900

    Home, TV, Search Container

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -13,7 +13,8 @@ export default () => (
       <Route path="/" exact component={Home} />
       <Route path="/tv" exact component={TV} />
       <Route path="/search" component={Search} />
-      <Route path="/detail" component={Detail} />
+      <Route path="/movie/:id" component={Detail} />
+      <Route path="/show/:id" component={Detail} />
       <Redirect from="*" to="/" />
     </Switch>
   </Router>
--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,23 +1,52 @@
 import React from "react"
 import SearchPresenter from "./SearchPresenter"
+import { movieApi, tvApi } from "../../api"
 
 export default class extends React.Component {
   state = {
-    movieResult: null,
-    tvResult: null,
+    movieResults: null,
+    tvResults: null,
     searchTerm: "",
     error: null,
     loading: false
   }
+
+  handleSubmit = () => {
+    const { searchTerm } = this.state
+    if (searchTerm !== "") {
+      this.searchByTerm()
+    }
+  }
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state
+    this.setState({ loading: true })
+    try {
+      const {
+        data: { results: movieResults }
+      } = await movieApi.search(searchTerm)
+      const {
+        data: { results: tvResults }
+      } = await tvApi.search(searchTerm)
+
+      this.setState({ movieResults, tvResults })
+    } catch {
+      this.setState({ error: "Can't find searching result" })
+    } finally {
+      this.setState({ loading: false })
+    }
+  }
+
   render() {
-    const { movieResult, tvResult, searchTerm, error, loading } = this.state
+    const { movieResults, tvResults, searchTerm, error, loading } = this.state
     return (
       <SearchPresenter
-        movieResult={movieResult}
-        tvResult={tvResult}
+        movieResults={movieResults}
+        tvResults={tvResults}
         searchTerm={searchTerm}
         error={error}
         loading={loading}
+        handleSubmit={this.handleSubmit}
       ></SearchPresenter>
     )
   }
