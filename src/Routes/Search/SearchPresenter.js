c1da997a13ac7d4b0eef168c5d475b074415c96a
JunHyeock Park <wnsgur6311@gmail.com>
2019-08-29 17:38:06 +0900

    Poster Components: CSS

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -4,6 +4,7 @@ import styled from "styled-components"
 import Loader from "Components/Loader"
 import Section from "Components/Section"
 import Message from "Components/Message"
+import Poster from "Components/Poster"
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -45,7 +46,15 @@ const SearchPresenter = ({
         {movieResults && movieResults.length > 0 && (
           <Section title="Movie Results">
             {movieResults.map((movie, key) => (
-              <span key={key}>{movie.title}</span>
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                imageUrl={movie.poster_path}
+                title={movie.original_title}
+                rating={movie.vote_average}
+                year={movie.release_date}
+                isMovie={true}
+              ></Poster>
             ))}
           </Section>
         )}
@@ -55,7 +64,15 @@ const SearchPresenter = ({
         {tvResults && tvResults.length > 0 && (
           <Section title="Show Results">
             {tvResults.map((show, key) => (
-              <span key={key}>{show.name}</span>
+              <Poster
+                key={show.id}
+                id={show.id}
+                imageUrl={show.poster_path}
+                title={show.original_name}
+                rating={show.vote_average}
+                year={show.first_air_date}
+                isMovie={false}
+              ></Poster>
             ))}
           </Section>
         )}
