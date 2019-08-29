941497589e48bd404119211dfb87ac26036297c5
JunHyeock Park <wnsgur6311@gmail.com>
2019-08-29 22:34:21 +0900

    Updates

---
+++ precache-manifest.c00fc76064a6db2f0ec485fb0ab38878.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "904f4cff2e5b0ddb45f59d46a8c2d603",
+    "url": "/nomflix/index.html"
+  },
+  {
+    "revision": "0419da838f0bd280cdd6",
+    "url": "/nomflix/static/js/2.436bf3dc.chunk.js"
+  },
+  {
+    "revision": "dffa194f7f5370c6295f",
+    "url": "/nomflix/static/js/main.a5d1a6d7.chunk.js"
+  },
+  {
+    "revision": "120d09f877dd835eab60",
+    "url": "/nomflix/static/js/runtime~main.c9bcd972.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nomflix/precache-manifest.89417b1b224804f9226ca407883c9bec.js"
+  "/nomflix/precache-manifest.c00fc76064a6db2f0ec485fb0ab38878.js"
 );
 
 self.addEventListener('message', (event) => {
